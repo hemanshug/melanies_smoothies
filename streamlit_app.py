64200d1bda058c4999116b2b386d0985c1b5fc24
hemanshug <48656584+hemanshug@users.noreply.github.com>
2026-01-27 12:36:11 +0100

    Add dataframe for smoothie fruit response
    Display smoothie fruit data in a dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,7 @@ ingredients_list = st.multiselect(
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
 
 if ingredients_list:
